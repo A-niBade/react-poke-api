24cb4dfb45e96c3e0cfc5105026aa86f1d3c9609
José Maldonado <joan.maldonadoa@gmail.com>
2022-09-07 14:34:16 -0300

    Added pokegrid

--- src/PokeApp.jsx
+++ src/PokeApp.jsx
@@ -1,6 +1,7 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Searchbar } from "./components/Searchbar";
+import { PokemonGrid } from "./components/PokemonGrid";
 
 import "../public/styles/style.css";
 
@@ -9,7 +10,7 @@ export const PokeApp = () => {
     <div>
       <Header />
       <Searchbar />
-
+      <PokemonGrid />
       <Footer />
     </div>
   );
