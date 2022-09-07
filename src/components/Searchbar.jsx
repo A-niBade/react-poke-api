430a61b8ba31a5d86d8c3968556345708e01c819
José Maldonado <joan.maldonadoa@gmail.com>
2022-09-07 14:34:04 -0300

    Input reset on submit

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+import { searchPokemon } from "../helpers/searchPokemon";
+
 export const Searchbar = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = await searchPokemon(inputValue);
+    console.log(data);
+    setInputValue("");
+  };
+
   return (
     <div className="searchbar-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Search Pokemon..."
           className="nes-input searchbar-input"
+          onChange={(event) => setInputValue(event.target.value)}
+          value={inputValue}
         />
-        <button type="button" className="nes-btn searchbar-btn">
+        <button type="submit" className="nes-btn searchbar-btn">
           Search
         </button>
       </form>
