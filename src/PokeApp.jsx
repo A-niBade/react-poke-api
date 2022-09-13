912e79bfa87e431d9932dc7b7ce8ea09312f453c
José Maldonado <joan.maldonadoa@gmail.com>
2022-09-12 21:40:34 -0300

    - Hooks added

--- src/PokeApp.jsx
+++ src/PokeApp.jsx
@@ -1,17 +1,48 @@
-import { Footer } from "./components/Footer";
+import { useState, useEffect } from "react";
+
 import { Header } from "./components/Header";
-import { Searchbar } from "./components/Searchbar";
-import { PokemonGrid } from "./components/PokemonGrid";
+import { Pokedex } from "./components/Pokedex";
+import { PokedexGrid } from "./components/PokedexGrid";
+import { getPokemons } from "./helpers/getPokemons";
+import { getPokemonData } from "./helpers/getPokemonData";
 
 import "../public/styles/style.css";
 
 export const PokeApp = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [pages, setPages] = useState();
+
+  const fetchPokemons = async () => {
+    try {
+      const data = await getPokemons(21, 21 * pages);
+      const promises = data.results.map(async (pokemon) => {
+        return await getPokemonData(pokemon.url);
+      });
+      const results = await Promise.all(promises);
+      setPokemons(results);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemons();
+  }, [pages]);
+
   return (
     <div>
       <Header />
-      <Searchbar />
-      <PokemonGrid />
-      <Footer />
+
+      <div className="main-container">
+        <Pokedex pages={pages} setPages={setPages} />
+        {loading ? (
+          <div>Cargando pokemones</div>
+        ) : (
+          <PokedexGrid pokemons={pokemons} />
+        )}
+      </div>
     </div>
   );
 };
