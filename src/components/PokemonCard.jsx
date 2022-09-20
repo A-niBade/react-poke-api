0bd3c9a745d188d39c2b9e765ef542b636448f6d
José Maldonado <joan.maldonadoa@gmail.com>
2022-09-20 13:02:30 -0300

    Added className with style

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,16 @@
 export const PokemonCard = ({ pokemon }) => {
+  const style = `pokemon-card ${pokemon.type}`;
+
+  console.log(style);
+
   return (
     <div className="pokemon-card">
       <div className="pokemon-card-img">
         <img src={pokemon.sprites.front_default} alt={pokemon.name} />
       </div>
 
-      <div>
-        <div className="pokemon-details">
+      <div className="pokemon-details">
+        <div className="pokemon-id">
           <h3>
             {pokemon.id}# {pokemon.name}
           </h3>
@@ -14,7 +18,9 @@ export const PokemonCard = ({ pokemon }) => {
 
         <div className="pokemon-type">
           {pokemon.types.map((type, idx) => (
-            <h5 key={idx}>{type.type.name}</h5>
+            <div key={idx} className={`pokemon-type-name ${type.type.name}`}>
+              {type.type.name}
+            </div>
           ))}
         </div>
       </div>
