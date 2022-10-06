b12c367a1991efd97af6da991f1a4f45c61bfb76
José Maldonado <joan.maldonadoa@gmail.com>
2022-10-05 23:59:30 -0300

    Changed import for PokeApp on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { PokeApp } from "./PokeApp";
+import { PokeApp } from "../src/pages/PokeApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
