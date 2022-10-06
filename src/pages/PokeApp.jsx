f784469593de62e006a392e112e569ed3890e1bc
José Maldonado <joan.maldonadoa@gmail.com>
2022-10-05 23:58:52 -0300

    Moved PokeApp component to pages folder

---
+++ src/pages/PokeApp.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+import { Pokedex, PokemonGrid } from "../components";
+import { getPokemons, getPokemonData, searchPokemon } from "../services";
+
+import "../../public/styles/style.css";
+
+export const PokeApp = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [pages, setPages] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const fetchPokemons = async () => {
+    try {
+      const data = await getPokemons(21, 21 * pages);
+      const totalPages = data.count / 21;
+      setTotal(totalPages.toFixed());
+      const promises = data.results.map(async (pokemon) => {
+        return await getPokemonData(pokemon.url);
+      });
+      const results = await Promise.all(promises);
+      setPokemons(results);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onSearch = async (pokemon) => {
+    setLoading(true);
+    const result = await searchPokemon(pokemon);
+    setPokemons([result]);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPokemons();
+  }, [pages]);
+
+  return (
+    <div>
+      <div className="poke-container">
+        <Pokedex
+          pages={pages}
+          setPages={setPages}
+          total={total}
+          onSearch={onSearch}
+        />
+        {loading ? (
+          <div>Cargando pokemones</div>
+        ) : (
+          <PokemonGrid pokemons={pokemons} />
+        )}
+      </div>
+    </div>
+  );
+};
